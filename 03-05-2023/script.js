3ebf7c8271cc9d4b6ca67caf0db4be54f5e672af
lentgiuric <132457549+lentgiuric@users.noreply.github.com>
2023-05-03 16:13:53 +0200

    Esercitazione 3  Maggio 2023

---
+++ 03-05-2023/script.js
@@ -0,0 +1,76 @@
+/* Sulla base della lezione del giorno, scrivere un piccolo programmino che preso un input da parte 
+dell'utente (prompt) di tipo number, verifichi se questo è pari o dispari e lo stampi in console. 
+Si utilizzi il costrutto if-else.
+
+Per verificare se un numero è pari o dispari può essere un compito un pò strano in matematica, 
+ma è molto ricorrente in programmazione (non è sempre così, matematica e programmazione 
+    (sopratutto nel front-end) non camminano necessariamente insieme, quindi tranquilli!). 
+    Utilizzate Google per cominciare a scovare una soluzione a questo problema 
+    (bastano gli operatori matematici!). */
+
+//            ESERCIZIO 1
+
+// let confNumber = parseInt(prompt("Inserisci il numero e ti dico se è PARI o DISPARI")) ;
+
+
+// if (confNumber%2 === 0) {
+//     alert("Il numero è pari");
+// } else {
+//     alert("Il numero è dispari");
+// }
+
+
+
+
+
+
+
+//                                        ESERCIZIO 2
+
+// Svolgere lo stesso esercizio utilizzando l'operatore ternario
+
+
+
+// confNumber%2 === 0  ? alert("Il numero è pari") : alert("Il numero è dispari");
+
+
+
+
+
+
+
+// ESERCIZIO AVANZATO
+
+
+// let firstNumber = parseInt(prompt("Inserisci il primo numero"));
+// let secondNumber = parseInt(prompt("Inserisci il secondo numero"));
+// let operator = prompt("inserisci un operatore matematico: +, -, *, /");
+
+
+
+// switch (operator) {
+//     case "+":
+//            console.log("La somma dei due numeri è: " + (firstNumber + secondNumber))
+           
+//         break;
+//     case "*":
+//            console.log("Il prodotto dei due numeri è: " + (firstNumber * secondNumber))
+//         break;
+//     case "/":
+//             console.log("Il quoziente dei due numeri è: " + (firstNumber / secondNumber))
+//          break;
+//     case "-":
+//             console.log("La sottrazione dei due numeri è: " + (firstNumber - secondNumber))
+//          break; 
+
+//     default:
+//         console.log("Inserisci un operatore richiesto!")
+//         break;
+// }
+
+
+
+
+
+
+
