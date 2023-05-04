2c3e356a8587cf60f2d75b66eba9ac9b25860838
lentgiuric <132457549+lentgiuric@users.noreply.github.com>
2023-05-04 17:38:22 +0200

    Esercitazione 4 Maggio 2023

---
+++ 04-05-2023/script.js
@@ -0,0 +1,37 @@
+function esercizio1() {
+
+    //Scrivere un piccolo script javascript che inverta un array senza utilizzare il metodo reverse().
+  
+      let arr = ["giovedi", "mercoledi", "martedi", "lunedi"];
+  
+  
+          for (let i = arr.length - 1; i >= 0; i--) {
+  
+      
+      }
+  }
+  
+
+function esercizio2() {
+
+// Dato un array di numeri, stampare il valore minimo e massimo
+
+    let contNum = [23, 56, 802, 44, 2, 65, 7, 11, 96];
+
+    let max = Math.max(...contNum);
+
+    let min = Math.min(...contNum);
+
+    console.log(min);
+    console.log(max);
+
+}
+
+
+esercizio1();
+esercizio2();
+
+
+
+
+
